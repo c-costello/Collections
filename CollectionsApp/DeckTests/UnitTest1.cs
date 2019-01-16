4a8cb6ad8810fa1608274a0817063bf8d2cfd670
Clarice Costello <claricostello@outlook.com>
2019-01-16 14:00:50 -0800

    Wrote CanRemoveCardFromDeck test

--- CollectionsApp/DeckTests/UnitTest1.cs
+++ CollectionsApp/DeckTests/UnitTest1.cs
@@ -7,13 +7,30 @@ namespace DeckTests
     public class UnitTest1
     {
         [Fact]
-        public void CanAddCardToMethod()
+        public void CanAddCardToDeck()
         {
             Deck<Card> deck = new Deck<Card>();
             deck.Add(new Card("Q", Card.Suit.Diamonds));
 
 
             Assert.Equal(Card.Suit.Diamonds, deck.internalItems[0].CardSuit);
+        }
+
+        [Fact]
+        public void CanRemoveCardFromDeck()
+        {
+            Deck<Card> deck = new Deck<Card>();
+            Card cardOne = new Card("Q", Card.Suit.Diamonds);
+            Card cardToBeRemoved = new Card("J", Card.Suit.Hearts);
+            Card cardThree = new Card("5", Card.Suit.Clubs);
+            deck.Add(cardOne);
+            deck.Add(cardToBeRemoved);
+            deck.Add(cardThree);
+            deck.Remove(cardToBeRemoved);
+
+
+            Assert.Equal("5", deck.internalItems[1].Value);
+
         }
     }
 }
