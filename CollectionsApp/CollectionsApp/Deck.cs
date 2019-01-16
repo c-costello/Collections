2bce9480274de0db223f447cb6abb57cba6f4ef6
Clarice Costello <claricostello@outlook.com>
2019-01-16 10:44:19 -0800

    built out remove function

--- CollectionsApp/CollectionsApp/Deck.cs
+++ CollectionsApp/CollectionsApp/Deck.cs
@@ -8,16 +8,41 @@ namespace CollectionsApp
     public class Deck<T> : IEnumerable<T>
     {
 
-        T[] internalItems = new T[5];
+        public T[] internalItems = new T[2];
         int currentIndex = 0;
         public void Add(T item)
         {
-            if (currentIndex < internalItems.Length)
+            if (currentIndex > internalItems.Length - 1)
             {
-                Array.Resize(ref internalItems, internalItems.Length * 2);
+                Array.Resize(ref internalItems, internalItems.Length + 1);
             }
             internalItems[currentIndex] = item;
             currentIndex++;
+            Console.WriteLine("Length" + internalItems.Length);
+        }
+
+        public void Remove(T item)
+        {
+            int j = 0;
+            for (int i = 0; i < internalItems.Length; i++)
+            {
+                if (item.Equals(internalItems[i]))
+                {
+                    j++;
+                }
+                internalItems[i] = internalItems[j];
+                j++;
+
+                if (j == internalItems.Length)
+                {
+                    i = internalItems.Length;
+                    Console.WriteLine("didnt break");
+                }
+
+            }
+            currentIndex--;
+            Array.Resize(ref internalItems, j - 1);
+            Console.WriteLine((internalItems.Length));
         }
 
 
@@ -31,7 +56,7 @@ namespace CollectionsApp
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return GetEnumerator()
+            return GetEnumerator();
         }
     }
 }
--- CollectionsApp/CollectionsApp/Program.cs
+++ CollectionsApp/CollectionsApp/Program.cs
@@ -10,6 +10,10 @@ namespace CollectionsApp
             Card cardOne = new Card('1', Card.Suit.Clubs);
             Card cardTwo = new Card('2', Card.Suit.Clubs);
             Card cardThree = new Card('3', Card.Suit.Clubs);
+            Card cardFour = new Card('4', Card.Suit.Clubs);
+            Card cardFive = new Card('5', Card.Suit.Clubs);
+            Card cardSix = new Card('6', Card.Suit.Clubs);
+            Card cardSeven = new Card('7', Card.Suit.Clubs);
             deck.Add(cardOne);
             deck.Add(cardTwo);
             deck.Add(cardThree);
@@ -17,7 +21,29 @@ namespace CollectionsApp
             {
                 Console.WriteLine(card.Value);
             }
+            deck.Remove(cardTwo);
 
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card.Value);
+            }
+
+            deck.Add(cardFour);
+            deck.Add(cardFive);
+            deck.Add(cardSix);
+            deck.Add(cardSeven);
+
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card.Value);
+            }
+
+            deck.Remove(cardFive);
+
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card.Value);
+            }
         }
     }
 }
